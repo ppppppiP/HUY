996ce0791b34429624c23631588c4d78745c1097
Поповичев Артём Алексеевич <67004012+ppppppiP@users.noreply.github.com>
2023-02-15 10:18:25 +0300

    Add files via upload

---
+++ RPGgame/HUY/Program.cs
@@ -0,0 +1,19 @@
+﻿using RPGworld1;
+
+int a = 5;
+if (a > 5)
+{
+    Console.WriteLine("Afa");
+}else if (a < 5)
+{
+    Console.WriteLine("dfg");
+}
+BackPack backPack = new(5);
+backPack.Looting(1);
+backPack.Looting(1);
+
+Hero hero = new("Hero", 10, 100);
+Enemy enemy = new("Дракон", 11, 150, 15);
+BattleArena arena = new(enemy, hero, backPack);
+arena.Battle();
+arena.Battle();
